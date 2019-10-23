6b07ad14a1be06ab9fc4c21f3069aa295c08300b
OnumaAyumi <onuayu.min@gmail.com>
2019-10-23 15:06:48 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,9 @@
 $(function(){ 
   function buildHTML(message){
-   var img = message.image == null ? "" : `<img src=${message.image} ></img>`
+   var img = message.image? `<img src=${message.image} ></img>` : ""
 
     var html =
-      `<div class="message" >
+      `<div class="message" data-message-id="${message.id}">
         <div class="message_upper-info">
           <p class="message_upper-info__talker">
             ${message.user_name}
@@ -46,4 +46,35 @@ $(function(){
       return false;
     });
   });
-})
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message').last().data('message-id');
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: 'api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      messages.forEach(function(message) {
+      
+        insertHTML += buildHTML(message);
+      
+    });
+    $('.messages').append(insertHTML);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+  })
+  .fail(function(data){
+    alert('自動更新に失敗しました。')
+  })
+  }
+  setInterval(reloadMessages, 5000);
+  }
+});
